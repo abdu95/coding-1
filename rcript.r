97c4742f81dd74cc361baba97bb6267ed4b2e165
Abduvosid Malikov <abdumalik2014@gmail.com>
2020-09-28 16:32:42 +0200

    add script file

---
+++ rcript.r
@@ -0,0 +1,24 @@
+2+3
+my_string <- "Hello City!"
+print(my_string)
+# We can define numbers
+a <- 2
+b <- 3
+
+a+b-(a*b)^a
+
+c <- a+b
+d <- a*c/b*c
+
+# Use of logical operators
+a == b
+2 == 3
+(a + 1) == b
+a <- 2
+a != b
+
+# other logical operators
+
+2 == 2 & 3 == 2
+
+2 == 2 | 3 == 2
